43c359120987ac09df2e6857f2d6d273596d1734
emilberwald <emilberwald@hotmail.com>
2022-01-29 18:19:16 +0100

    Add newer version..

--- unity/AutomaticColliderFitting.cs
+++ unity/AutomaticColliderFitting.cs
@@ -5,7 +5,61 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using Unity.EditorCoroutines.Editor;
+using System;
+using System.Reflection;
 
+public static class AutomaticColliderFittingExtensions
+{
+    internal static int GetDirection(this CapsuleCollider collider, Vector3 worldVertex)
+    {
+        float x = Math.Min(Vector3.Angle(collider.transform.right, worldVertex - collider.transform.position), Vector3.Angle(-collider.transform.right, worldVertex - collider.transform.position));
+        float y = Math.Min(Vector3.Angle(collider.transform.up, worldVertex - collider.transform.position), Vector3.Angle(-collider.transform.up, worldVertex - collider.transform.position));
+        float z = Math.Min(Vector3.Angle(collider.transform.forward, worldVertex - collider.transform.position), Vector3.Angle(-collider.transform.forward, worldVertex - collider.transform.position));
+        if (x <= Math.Min(y, z))
+        {
+            return 0;
+        }
+        else if (y <= Math.Min(x, z))
+        {
+            return 1;
+        }
+        else if (z <= Math.Min(x, y))
+        {
+            return 2;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    internal static void UpdateRadius(this CapsuleCollider collider, Vector3 worldVertex)
+    {
+        Debug.Log($"Using radius {collider.transform.position} -> {worldVertex} with distance {(worldVertex - collider.transform.position).magnitude}.", collider.gameObject);
+        Debug.DrawRay(collider.transform.position, worldVertex - collider.transform.position, Color.magenta, 60);
+        collider.radius = collider.transform.InverseTransformPoint(worldVertex).magnitude;
+    }
+    internal static void UpdateHeight(this CapsuleCollider collider, Vector3 worldVertex)
+    {
+        Debug.Log($"Using height {collider.transform.position} -> {worldVertex} with distance {(worldVertex - collider.transform.position).magnitude}.", collider.gameObject);
+        Debug.DrawRay(collider.transform.position, worldVertex - collider.transform.position, Color.cyan, 60);
+        collider.height = collider.transform.InverseTransformPoint(worldVertex).magnitude;
+    }
+    internal static void ReflectionRestoreToBindPose(this GameObject gameObject)
+    {
+        if (gameObject == null)
+            return;
+        Type type = Type.GetType("UnityEditor.AvatarSetupTool, UnityEditor");
+        if (type != null)
+        {
+            MethodInfo info = type.GetMethod("SampleBindPose", BindingFlags.Static | BindingFlags.Public);
+            if (info != null)
+            {
+                info.Invoke(null, new object[] { gameObject });
+            }
+        }
+    }
+}
 
 public class AutomaticColliderFitting : ScriptableWizard
 {
@@ -13,7 +67,10 @@ public class AutomaticColliderFitting : ScriptableWizard
     public SkinnedMeshRenderer SkinnedMeshRenderer { get; set; }
 
     [field: SerializeField]
-    public Cloth Cloth { get; set; }
+    public Cloth[] Clothes { get; set; }
+
+    [field: SerializeField]
+    public float ScaleFactor { get; set; } = 1.0f;
 
     [MenuItem("Tools/Fit Colliders To Bones")]
     static void CreateWizard()
@@ -29,73 +86,58 @@ public class AutomaticColliderFitting : ScriptableWizard
 
         List<CapsuleCollider> colliders = new List<CapsuleCollider>();
 
+        this.SkinnedMeshRenderer.gameObject.ReflectionRestoreToBindPose();
+
         var meshCollider = this.SkinnedMeshRenderer.transform.root.gameObject.AddComponent<MeshCollider>();
         try
         {
             Physics.queriesHitBackfaces = true;
 
-            meshCollider.sharedMesh = this.SkinnedMeshRenderer.sharedMesh;
+            Mesh colliderMesh = new Mesh();
+            this.SkinnedMeshRenderer.BakeMesh(colliderMesh);
+            meshCollider.sharedMesh = null;
+            meshCollider.sharedMesh = colliderMesh;
 
-            var maxDistance = this.SkinnedMeshRenderer.bounds.size.magnitude;
-
-            foreach(var bone in this.SkinnedMeshRenderer.bones)
+            foreach (var bone in this.SkinnedMeshRenderer.bones)
             {
-                if(bone == null) continue;
-
-                List<Vector3> X = new List<Vector3> { Vector3.left, Vector3.right };
-                List<Vector3> Y = new List<Vector3> { Vector3.up, Vector3.down };
-                List<Vector3> Z = new List<Vector3> { Vector3.forward, Vector3.back };
-
-                var resultX = RayCast(meshCollider, maxDistance, bone, X).ToList();
-                var resultY = RayCast(meshCollider, maxDistance, bone, Y).ToList();
-                var resultZ = RayCast(meshCollider, maxDistance, bone, Z).ToList();
-
-                var closestToBoneX = resultX.Where(x => x.hit.collider != null).Select(x => (x.hit.distance, x)).DefaultIfEmpty().Min().x;
-                var closestToBoneY = resultY.Where(x => x.hit.collider != null).Select(x => (x.hit.distance, x)).DefaultIfEmpty().Min().x;
-                var closestToBoneZ = resultZ.Where(x => x.hit.collider != null).Select(x => (x.hit.distance, x)).DefaultIfEmpty().Min().x;
-
-                var tentative = new List<(Vector3 direction, Ray ray, RaycastHit hit)> { closestToBoneX, closestToBoneY, closestToBoneZ }.Where(x => x.direction != default).ToList();
-                if(tentative.Count < 2) { continue; }
-
-                var closest = tentative.Select(x => (x.hit.distance, x)).Min().x;
-                tentative.Remove(closest);
-                var furthest = tentative.Select(x => (x.hit.distance, x)).Max().x;
-
-
+                if (bone == null) continue;
                 var collider = Undo.AddComponent<CapsuleCollider>(bone.gameObject);
-                collider.center = Vector3.zero;
-                collider.radius = collider.transform.InverseTransformPoint(closest.hit.point).magnitude;
-                collider.height = collider.transform.InverseTransformPoint(furthest.hit.point).magnitude;
-                if(furthest.direction == Vector3.left || furthest.direction == Vector3.right) { collider.direction = 0; }
-                if(furthest.direction == Vector3.up || furthest.direction == Vector3.down) { collider.direction = 1; }
-                if(furthest.direction == Vector3.forward || furthest.direction == Vector3.back) { collider.direction = 2; }
 
-                bool overlapped = Physics.ComputePenetration(
-                    collider, collider.transform.position, collider.transform.rotation,
-                    meshCollider, meshCollider.transform.position, meshCollider.transform.rotation,
-                    out Vector3 direction, out float distance
-                );
-
-                if(overlapped)
+                if (UpdateCollider(meshCollider, collider))
                 {
-                    Selection.objects = default;
-                    Selection.activeObject = collider;
-                    SceneView.lastActiveSceneView.LookAtDirect(collider.transform.position, SceneView.lastActiveSceneView.rotation);
-                    SceneView.RepaintAll();
-                    yield return null;
+                    bool overlapped = Physics.ComputePenetration(
+                        collider, collider.transform.position, collider.transform.rotation,
+                        meshCollider, meshCollider.transform.position, meshCollider.transform.rotation,
+                        out Vector3 _, out float _
+                    );
 
-                    if(EditorUtility.DisplayDialog("CapsuleCollider overlaps MeshCollider.", "Action for CapsuleCollider?", "Add", "Remove"))
+                    if (overlapped)
                     {
-                        colliders.Add(collider);
-                    }
-                    else
-                    {
-                        DestroyImmediate(collider);
+                        Debug.Log($"Overlapping {collider.radius} {collider.height}.", collider.gameObject);
+                        Selection.objects = default;
+                        Selection.activeObject = collider;
+                        SceneView.lastActiveSceneView.LookAtDirect(collider.transform.position, SceneView.lastActiveSceneView.rotation);
+                        SceneView.RepaintAll();
+                        yield return null;
+
+                        if (EditorUtility.DisplayDialog("CapsuleCollider overlaps MeshCollider.", "Action for CapsuleCollider?", "Add", "Remove"))
+                        {
+                            collider.radius *= this.ScaleFactor;
+                            collider.height *= this.ScaleFactor;
+                            colliders.Add(collider);
+                            continue;
+                        }
+                        else
+                        {
+                            DestroyImmediate(collider);
+                            continue;
+                        }
                     }
                 }
                 else
                 {
-                    colliders.Add(collider);
+                    DestroyImmediate(collider);
+                    continue;
                 }
             }
         }
@@ -106,21 +148,55 @@ public class AutomaticColliderFitting : ScriptableWizard
             Physics.queriesHitBackfaces = queriesHitBackfaces;
         }
 
-        if(this.Cloth != null)
+        if (this.Clothes != null)
         {
-            this.Cloth.capsuleColliders = colliders.ToArray();
-        }
-
-        static IEnumerable<(Vector3 direction, Ray ray, RaycastHit hit)> RayCast(MeshCollider meshCollider, float maxDistance, Transform bone, List<Vector3> X)
-        {
-            foreach(var direction in X)
+            foreach (var cloth in this.Clothes)
             {
-                var ray = new Ray(bone.position, bone.TransformDirection(direction));
-                meshCollider.Raycast(ray, out RaycastHit raycastHit, maxDistance);
-                yield return (direction, ray, raycastHit);
+                cloth.capsuleColliders = colliders.ToArray();
             }
         }
     }
+
+    private static bool UpdateCollider(MeshCollider meshCollider, CapsuleCollider collider)
+    {
+        List<RaycastHit> raycastHits = GetRayCastHits(meshCollider, collider);
+        if (raycastHits.Any())
+        {
+            var firstDirectionClosest = raycastHits.OrderBy(x => x.distance).FirstOrDefault();
+            collider.UpdateRadius(firstDirectionClosest.point);
+            int firstIgnoreDirection = collider.GetDirection(firstDirectionClosest.point);
+            raycastHits.RemoveAll(x => collider.GetDirection(x.point) == firstIgnoreDirection);
+            if (raycastHits.Any())
+            {
+                var secondDirectionClosest = raycastHits.OrderBy(x => x.distance).FirstOrDefault();
+                int secondIgnoreDirection = collider.GetDirection(secondDirectionClosest.point);
+                raycastHits.RemoveAll(x => collider.GetDirection(x.point) == secondIgnoreDirection);
+                if (raycastHits.Any())
+                {
+                    var thirdDirectionClosest = raycastHits.OrderBy(x => x.distance).FirstOrDefault();
+                    collider.UpdateHeight(thirdDirectionClosest.point);
+                    collider.direction = collider.GetDirection(thirdDirectionClosest.point);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static List<RaycastHit> GetRayCastHits(MeshCollider meshCollider, CapsuleCollider collider)
+    {
+        List<RaycastHit> raycastHits = new List<RaycastHit>();
+        foreach (var direction in new[] { collider.transform.up, collider.transform.right, collider.transform.forward, -collider.transform.up, -collider.transform.right, -collider.transform.forward })
+        {
+            if (meshCollider.Raycast(new Ray(collider.transform.position, collider.transform.TransformDirection(direction)), out RaycastHit hit, meshCollider.bounds.size.magnitude))
+            {
+                raycastHits.Add(hit);
+            }
+        }
+
+        return raycastHits;
+    }
+
 
     /// <summary>
     /// Fit
