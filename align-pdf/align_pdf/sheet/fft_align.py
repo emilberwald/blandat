846dcc6338f2071c8d649eb46fe3ca9a2bed68ee
emilberwald <emilberwald@hotmail.com>
2022-09-27 11:29:58 +0200

    Add random junk lying around

--- align-pdf/align_pdf/sheet/fft_align.py
+++ align-pdf/align_pdf/sheet/fft_align.py
@@ -8,17 +8,40 @@ from align_pdf.sheet.utils import rotate_image
 
 
 def fft_align(input_path: pathlib.Path, output_path: pathlib.Path):
-    for file in tqdm.tqdm(
-        (sorted((file for file in input_path.glob("**/*") if file.is_file()), key=lambda path: str(path)))
+    for file_no, file in enumerate(
+        tqdm.tqdm((sorted((file for file in input_path.glob("**/*") if file.is_file()), key=lambda path: str(path))))
     ):
         file: pathlib.Path
         output_path.mkdir(exist_ok=True)
         try:
             image = cv2.imread(str(file))
             image = align_image(image)
+            image = crop_image(image, pad_on_left=(file_no % 2) == 1)
             cv2.imwrite(str(output_path / file.with_suffix(".png").name), image)
         except Exception as e:
             logging.error(e)
+
+
+def crop_image(image, pad_on_left):
+    is_color = len(image.shape) == 3
+    grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) if is_color else image
+
+    _threshold, grayscale = cv2.threshold(grayscale, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    notePixelContours = cv2.findNonZero(255 - grayscale)
+    x, y, w, h = cv2.boundingRect(notePixelContours)
+    cropped_image = image[y : y + h, x : x + w]
+
+    # A4 w 210 h 297
+    # 1 cm margin above and below is ca 4% of height
+    # margin should be 20-25 mm, 10% of width for holes
+    vertical = int(0.04 * h)
+    left = int(0.1 * w) if pad_on_left else 0
+    right = int(0.1 * w) if not pad_on_left else 0
+    cropped_image = cv2.copyMakeBorder(
+        cropped_image, vertical, vertical, left, right, cv2.BORDER_CONSTANT, value=(255, 255, 255) if is_color else 255
+    )
+
+    return cropped_image
 
 
 def align_image(image):
